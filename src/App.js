baffcd396cf4812abd3c9a5d93a3e7d937e4c24b
Zoran G <68257343+verona-hub@users.noreply.github.com>
2022-06-18 21:03:45 +0200

    created MyStateManagement.js and ContextAPI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+// CSS
 import './App.css';
+// Context
+import { MyStateManagement } from './components/Context/MyStateManagement';
 
 
 const App = () => {
-  return (
-      <div>
-        This is my App
-      </div>
-  );
+    return (
+        <MyStateManagement.Provider vaue={{
+
+        }}>
+            <div className="App">
+                This is my App
+            </div>
+        </MyStateManagement.Provider>
+    );
 };
 
 
